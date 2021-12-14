64f8824a2c855e570bdf3e0cb5cbfa21f5830727
Marvin Chin Yi Kai <u2000490@siswa.um.edu.my>
2021-12-14 17:40:05 +0800

    Tidying up code

--- src/TTE/SameBehavior.java
+++ src/TTE/SameBehavior.java
@@ -1,15 +1,12 @@
 /*
 package TTE;
 public class SameBehavior {
-    //To check if attack, crit and block is successful or not
-    public boolean chance(double x){
-        double r = Math.random()*100;
-        return r >= (1 - x);
+
     }
-    
-    
-    
+ 
 }
 */
 
 //still in progress, will make it once done Game_Logic
+//To do -create abstract method
+//To Tower n Dragon extends sameBehavior()
