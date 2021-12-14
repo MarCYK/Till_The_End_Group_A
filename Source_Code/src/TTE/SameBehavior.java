def5c3dac173611a23d4f08074f87cac59478c0d
Marvin Chin Yi Kai <u2000490@siswa.um.edu.my>
2021-12-14 17:09:16 +0800

    Tidying up code

--- Source_Code/src/TTE/SameBehavior.java
+++ Source_Code/src/TTE/SameBehavior.java
@@ -1,14 +1,9 @@
 /*
 package TTE;
 public class SameBehavior {
-    //To check if attack, crit and block is successful or not
-    public boolean chance(double x){
-        double r = Math.random()*100;
-        return r >= (1 - x);
+
     }
-    
-    
-    
+
 }
 */
 
